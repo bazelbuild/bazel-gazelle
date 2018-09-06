82d15888a19178195d56d9a288a2653d16436b07
Jay Conrod <jayconrod@gmail.com>
2018-09-06 17:01:47 -0400

    Support go modules with update-repos -from_file=go.mod (#310)
    With this change, Gazelle can import modules listed in go.mod files
    (including transitive dependencies that are not listed) as
    go_repository rules. Gazelle runs "go list -m -json all" to obtain a
    list of modules, transforms the list into go_repository rules, then
    merges those rules into WORKSPACE.
    
    If Gazelle is invoked by the wrapper script generated by the gazelle
    rule, it uses the go tool from the downloaded SDK. Otherwise, it uses
    the SDK pointed to by GOROOT or whatever's in PATH.
    
    Fixes #200

--- internal/repos/import_test.go
+++ internal/repos/import_test.go
@@ -25,14 +25,18 @@ import (
 	"github.com/bazelbuild/bazel-gazelle/internal/rule"
 )
 
-func TestImportDep(t *testing.T) {
-	dir, err := ioutil.TempDir(os.Getenv("TEST_TEMPDIR"), "TestImportDep")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(dir)
-	lockFilename := filepath.Join(dir, "Gopkg.lock")
-	lockContent := []byte(`
+func init() {
+	goListModulesFn = goListModulesStub
+}
+
+func TestImports(t *testing.T) {
+	for _, tc := range []struct {
+		desc, filename, content, want string
+	}{
+		{
+			desc:     "dep",
+			filename: "Gopkg.lock",
+			content: `
 # This is an abbreviated version of dep's Gopkg.lock
 # Retrieved 2017-12-20
 
@@ -67,21 +71,8 @@ func TestImportDep(t *testing.T) {
   inputs-digest = "05c1cd69be2c917c0cc4b32942830c2acfa044d8200fdc94716aae48a8083702"
   solver-name = "gps-cdcl"
   solver-version = 1
-`)
-	if err := ioutil.WriteFile(lockFilename, lockContent, 0666); err != nil {
-		t.Fatal(err)
-	}
-
-	rules, err := ImportRepoRules(lockFilename)
-	if err != nil {
-		t.Fatal(err)
-	}
-	f := rule.EmptyFile("test", "")
-	for _, r := range rules {
-		r.Insert(f)
-	}
-	got := strings.TrimSpace(string(f.Format()))
-	want := strings.TrimSpace(`
+`,
+			want: `
 go_repository(
     name = "com_github_armon_go_radix",
     commit = "4239b77079c7b5d1243b7b4736304ce8ddb6f0f2",
@@ -106,8 +97,154 @@ go_repository(
     commit = "66aacef3dd8a676686c7ae3716979581e8b03c47",
     importpath = "golang.org/x/net",
 )
-`)
-	if got != want {
-		t.Errorf("got %s ; want %s", got, want)
+`,
+		}, {
+			desc:     "modules",
+			filename: "go.mod",
+			content: `
+module github.com/bazelbuild/bazel-gazelle
+
+require (
+	github.com/BurntSushi/toml v0.3.0 // indirect
+	github.com/bazelbuild/buildtools v0.0.0-20180226164855-80c7f0d45d7e
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pelletier/go-toml v1.0.1
+	golang.org/x/tools v0.0.0-20170824195420-5d2fd3ccab98
+	gopkg.in/yaml.v2 v2.2.1 // indirect
+)
+`,
+			want: `
+go_repository(
+    name = "com_github_bazelbuild_buildtools",
+    commit = "80c7f0d45d7e",
+    importpath = "github.com/bazelbuild/buildtools",
+)
+
+go_repository(
+    name = "com_github_burntsushi_toml",
+    importpath = "github.com/BurntSushi/toml",
+    tag = "v0.3.0",
+)
+
+go_repository(
+    name = "com_github_davecgh_go_spew",
+    importpath = "github.com/davecgh/go-spew",
+    tag = "v1.1.0",
+)
+
+go_repository(
+    name = "com_github_pelletier_go_toml",
+    importpath = "github.com/pelletier/go-toml",
+    tag = "v1.0.1",
+)
+
+go_repository(
+    name = "in_gopkg_check_v1",
+    commit = "20d25e280405",
+    importpath = "gopkg.in/check.v1",
+)
+
+go_repository(
+    name = "in_gopkg_yaml_v2",
+    importpath = "gopkg.in/yaml.v2",
+    tag = "v2.2.1",
+)
+
+go_repository(
+    name = "org_golang_x_tools",
+    commit = "5d2fd3ccab98",
+    importpath = "golang.org/x/tools",
+)
+`,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			dir, err := ioutil.TempDir(os.Getenv("TEST_TEMPDIR"), "TestImports")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.RemoveAll(dir)
+			filename := filepath.Join(dir, tc.filename)
+			if err := ioutil.WriteFile(filename, []byte(tc.content), 0666); err != nil {
+				t.Fatal(err)
+			}
+
+			rules, err := ImportRepoRules(filename)
+			if err != nil {
+				t.Fatal(err)
+			}
+			f := rule.EmptyFile("test", "")
+			for _, r := range rules {
+				r.Insert(f)
+			}
+			got := strings.TrimSpace(string(f.Format()))
+			want := strings.TrimSpace(tc.want)
+			if got != want {
+				t.Errorf("got:\n%s\n\nwant:\n%s\n", got, want)
+			}
+		})
 	}
+}
+
+func goListModulesStub(dir string) ([]byte, error) {
+	return []byte(`{
+	"Path": "github.com/bazelbuild/bazel-gazelle",
+	"Main": true,
+	"Dir": "/tmp/tmp.XxZ9HCw1Mq",
+	"GoMod": "/tmp/tmp.XxZ9HCw1Mq/go.mod"
+}
+{
+	"Path": "github.com/BurntSushi/toml",
+	"Version": "v0.3.0",
+	"Time": "2017-03-28T06:15:53Z",
+	"Indirect": true,
+	"Dir": "/usr/local/google/home/jayconrod/go/pkg/mod/github.com/!burnt!sushi/toml@v0.3.0",
+	"GoMod": "/usr/local/google/home/jayconrod/go/pkg/mod/cache/download/github.com/!burnt!sushi/toml/@v/v0.3.0.mod"
+}
+{
+	"Path": "github.com/bazelbuild/buildtools",
+	"Version": "v0.0.0-20180226164855-80c7f0d45d7e",
+	"Time": "2018-02-26T16:48:55Z",
+	"Dir": "/usr/local/google/home/jayconrod/go/pkg/mod/github.com/bazelbuild/buildtools@v0.0.0-20180226164855-80c7f0d45d7e",
+	"GoMod": "/usr/local/google/home/jayconrod/go/pkg/mod/cache/download/github.com/bazelbuild/buildtools/@v/v0.0.0-20180226164855-80c7f0d45d7e.mod"
+}
+{
+	"Path": "github.com/davecgh/go-spew",
+	"Version": "v1.1.0",
+	"Time": "2016-10-29T20:57:26Z",
+	"Indirect": true,
+	"Dir": "/usr/local/google/home/jayconrod/go/pkg/mod/github.com/davecgh/go-spew@v1.1.0",
+	"GoMod": "/usr/local/google/home/jayconrod/go/pkg/mod/cache/download/github.com/davecgh/go-spew/@v/v1.1.0.mod"
+}
+{
+	"Path": "github.com/pelletier/go-toml",
+	"Version": "v1.0.1",
+	"Time": "2017-09-24T18:42:18Z",
+	"Dir": "/usr/local/google/home/jayconrod/go/pkg/mod/github.com/pelletier/go-toml@v1.0.1",
+	"GoMod": "/usr/local/google/home/jayconrod/go/pkg/mod/cache/download/github.com/pelletier/go-toml/@v/v1.0.1.mod"
+}
+{
+	"Path": "golang.org/x/tools",
+	"Version": "v0.0.0-20170824195420-5d2fd3ccab98",
+	"Time": "2017-08-24T19:54:20Z",
+	"Dir": "/usr/local/google/home/jayconrod/go/pkg/mod/golang.org/x/tools@v0.0.0-20170824195420-5d2fd3ccab98",
+	"GoMod": "/usr/local/google/home/jayconrod/go/pkg/mod/cache/download/golang.org/x/tools/@v/v0.0.0-20170824195420-5d2fd3ccab98.mod"
+}
+{
+	"Path": "gopkg.in/check.v1",
+	"Version": "v0.0.0-20161208181325-20d25e280405",
+	"Time": "2016-12-08T18:13:25Z",
+	"Indirect": true,
+	"Dir": "/usr/local/google/home/jayconrod/go/pkg/mod/gopkg.in/check.v1@v0.0.0-20161208181325-20d25e280405",
+	"GoMod": "/usr/local/google/home/jayconrod/go/pkg/mod/cache/download/gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod"
+}
+{
+	"Path": "gopkg.in/yaml.v2",
+	"Version": "v2.2.1",
+	"Time": "2018-03-28T19:50:20Z",
+	"Indirect": true,
+	"Dir": "/usr/local/google/home/jayconrod/go/pkg/mod/gopkg.in/yaml.v2@v2.2.1",
+	"GoMod": "/usr/local/google/home/jayconrod/go/pkg/mod/cache/download/gopkg.in/yaml.v2/@v/v2.2.1.mod"
+}
+`), nil
 }
