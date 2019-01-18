a5059b7a22067d576ad333c1906b98327a607ebf
vam <vam@google.com>
2019-01-17 17:16:35 -0800

    Update file header year from 2018 to 2019

--- language/proto/gen/update_proto_csv_from_query.go
+++ language/proto/gen/update_proto_csv_from_query.go
@@ -1,6 +1,6 @@
 // +build ignore
 
-/* Copyright 2018 The Bazel Authors. All rights reserved.
+/* Copyright 2019 The Bazel Authors. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
