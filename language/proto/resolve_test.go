8c77987f4c2c5d3a22af5546641f22fd5a481294
Danny Wolf <930169+wolfd@users.noreply.github.com>
2021-09-17 12:23:45 -0700

    Test resolve behavior in file mode

--- language/proto/resolve_test.go
+++ language/proto/resolve_test.go
@@ -351,6 +351,41 @@ proto_library(
     deps = ["//foo:foo_proto"],
 )
 `,
+		}, {
+			desc: "test single file resolution in file mode",
+			index: []buildFile{{
+				rel: "foo",
+				content: `
+# gazelle:proto file
+
+proto_library(
+    name = "foo_proto",
+    srcs = ["foo.proto"],
+)
+
+proto_library(
+    name = "bar_proto",
+    srcs = ["bar.proto"],
+)
+
+proto_library(
+    name = "baz_proto",
+    srcs = ["baz.proto"],
+)
+`,
+			}},
+			old: `
+proto_library(
+    name = "other_proto",
+    _imports = ["foo/bar.proto"],
+)
+`,
+			want: `
+proto_library(
+    name = "other_proto",
+    deps = ["//foo:bar_proto"],
+)
+`,
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
