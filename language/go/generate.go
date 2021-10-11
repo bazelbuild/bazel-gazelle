bazelbuild/bazel-gazelle

commit ce6d56176c9d2d2714aaaf57cf4ce755de6bfeee

language/go: stop generating go_tool_library rules for x/tools (#1110)
bazelbuild/rules_go#2374 has been fixed, so there is no more need for it.

language/go/generate.go
@@ -273,9 +273,6 @@ func (gl *goLang) GenerateRules(args language.GenerateArgs) language.GenerateRes
 			libName = lib.Name()
 		}
 		rules = append(rules, lib)
-		if r := g.maybeGenerateToolLib(lib, pkg); r != nil {
-			rules = append(rules, r)
-		}
 		if r := g.maybeGenerateExtraLib(lib, pkg); r != nil {
 			rules = append(rules, r)
 		}
@@ -533,68 +530,6 @@ func (g *generator) generateTest(pkg *goPackage, library string) *rule.Rule {
 		goTest.SetAttr("data", rule.GlobValue{Patterns: []string{"testdata/**"}})
 	}
 	return goTest
-}
-
-// maybeGenerateToolLib generates a go_tool_library target equivalent to the
-// go_library in the same directory. maybeGenerateToolLib returns nil for
-// packages outside golang.org/x/tools and for packages that aren't known
-// dependencies of nogo.
-//
-// HACK(#834): This is only needed by golang.org/x/tools for dependencies of
-// nogo. go_tool_library should be removed when bazelbuild/rules_go#2374 is
-// resolved, so these targets shouldn't be generated in other repositories.
-// Generating them here automatically makes it easier to upgrade
-// org_golang_x_tools.
-func (g *generator) maybeGenerateToolLib(lib *rule.Rule, pkg *goPackage) *rule.Rule {
-	// Check whether we should generate go_tool_library.
-	gc := getGoConfig(g.c)
-	if gc.prefix != "golang.org/x/tools" || gc.prefixRel != "" || !isToolLibImportPath(pkg.importPath) {
-		return nil
-	}
-
-	// Generate the target.
-	toolLib := rule.NewRule("go_tool_library", "go_tool_library")
-	var visibility []string
-	if pkg.importPath == "golang.org/x/tools/go/analysis/internal/facts" {
-		// Imported by nogo main. We add a visibility exception.
-		visibility = []string{"//visibility:public"}
-	} else {
-		visibility = g.commonVisibility(pkg.importPath)
-	}
-	g.setCommonAttrs(toolLib, pkg.rel, visibility, pkg.library, "")
-	g.setImportAttrs(toolLib, pkg.importPath)
-	return toolLib
-}
-
-func isToolLibImportPath(imp string) bool {
-	if !strings.HasPrefix(imp, "golang.org/x/tools/") {
-		return false
-	}
-	pass := strings.TrimPrefix(imp, "golang.org/x/tools/go/analysis/passes/")
-	if pass != imp && strings.Index(pass, "/") < 0 {
-		// Direct dependency of nogo
-		return true
-	}
-	switch imp {
-	case "golang.org/x/tools/go/analysis",
-		"golang.org/x/tools/go/analysis/internal/facts",
-		"golang.org/x/tools/go/analysis/passes/internal/analysisutil",
-		"golang.org/x/tools/go/ast/astutil",
-		"golang.org/x/tools/go/ast/inspector",
-		"golang.org/x/tools/go/cfg",
-		"golang.org/x/tools/go/gcexportdata",
-		"golang.org/x/tools/go/internal/gcimporter",
-		"golang.org/x/tools/go/ssa",
-		"golang.org/x/tools/go/types/objectpath",
-		"golang.org/x/tools/go/types/typeutil",
-		"golang.org/x/tools/internal/analysisinternal",
-		"golang.org/x/tools/internal/typeparams",
-		"golang.org/x/tools/internal/lsp/fuzzy":
-		// Indirect dependency of nogo.
-		return true
-	default:
-		return false
-	}
 }
 
 // maybeGenerateExtraLib generates extra equivalent library targets for

language/go/resolve.go
@@ -74,8 +74,6 @@ func (gl *goLang) Resolve(c *config.Config, ix *resolve.RuleIndex, rc *repo.Remo
 	switch r.Kind() {
 	case "go_proto_library":
 		resolve = resolveProto
-	case "go_tool_library":
-		resolve = resolveGoTool
 	default:
 		resolve = ResolveGo
 	}
@@ -328,19 +326,6 @@ func resolveProto(c *config.Config, ix *resolve.RuleIndex, rc *repo.RemoteCache,
 	}
 	libName := libNameByConvention(getGoConfig(c).goNamingConvention, imp, "")
 	return label.New("", rel, libName), nil
-}
-
-func resolveGoTool(c *config.Config, ix *resolve.RuleIndex, rc *repo.RemoteCache, imp string, from label.Label) (label.Label, error) {
-	if isToolLibImportPath(imp) {
-		gc := getGoConfig(c)
-		var repo string
-		if gc.prefix != "golang.org/x/tools" {
-			repo = "org_golang_x_tools"
-		}
-		pkg := strings.TrimPrefix(imp, "golang.org/x/tools/")
-		return label.Label{Repo: repo, Pkg: pkg, Name: "go_tool_library"}, nil
-	}
-	return ResolveGo(c, ix, rc, imp, from)
 }
 
 // wellKnownProtos is the set of proto sets for which we don't need to add

commit 3b8b66b9579bda99a3301c61dd4fe46e714845c7

language/go: set golang.org/x/tools/go/analysis/internal/facts to public visibility
ce6d56176c9d2d2714aaaf57cf4ce755de6bfeee broke configurations which
bring their own x/tools go_repository and use nogo, because the
rules_go public visibility patch is lost and nogo's dependency on the
internal/facts package breaks the build.

Having Gazelle set that package's go_library to public seems to be the
best fix here.

language/go/generate.go
@@ -273,10 +273,12 @@ func (gl *goLang) GenerateRules(args language.GenerateArgs) language.GenerateRes
 			libName = lib.Name()
 		}
 		rules = append(rules, lib)
+		g.maybePublishToolLib(lib, pkg)
 		if r := g.maybeGenerateExtraLib(lib, pkg); r != nil {
 			rules = append(rules, r)
 		}
 		if r := g.maybeGenerateAlias(pkg, libName); r != nil {
+			g.maybePublishToolLib(r, pkg)
 			rules = append(rules, r)
 		}
 		rules = append(rules,
@@ -530,6 +532,15 @@ func (g *generator) generateTest(pkg *goPackage, library string) *rule.Rule {
 		goTest.SetAttr("data", rule.GlobValue{Patterns: []string{"testdata/**"}})
 	}
 	return goTest
+}
+
+// maybePublishToolLib makes the given go_library rule public if needed for nogo.
+// Updating it here automatically makes it easier to upgrade org_golang_x_tools.
+func (g *generator) maybePublishToolLib(lib *rule.Rule, pkg *goPackage) {
+	if pkg.importPath == "golang.org/x/tools/go/analysis/internal/facts" {
+		// Imported by nogo main. We add a visibility exception.
+		lib.SetAttr("visibility", []string{"//visibility:public"})
+	}
 }
 
 // maybeGenerateExtraLib generates extra equivalent library targets for