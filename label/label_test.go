890dcf68078aa0d4268709853d050858d4a64c56
Daniel Wagner-Hall <dwagnerhall@apple.com>
2022-06-09 17:39:28 +0100

    Don't exclude spaces from the label name regex
    In parsing the output of `bazel query`, we found that some files with
    spaces in their names are included in the output.
    
    It seems reasonable that all labels that come out of a `bazel query`
    should be parsable by this `Label` type; let's just use the raw regex
    from the Bazel documentation.

--- label/label_test.go
+++ label/label_test.go
@@ -81,6 +81,7 @@ func TestParse(t *testing.T) {
 		{str: "//api_proto:api.gen.pb.go_checkshtest", want: Label{Pkg: "api_proto", Name: "api.gen.pb.go_checkshtest"}},
 		{str: "@go_sdk//:src/cmd/go/testdata/mod/rsc.io_!q!u!o!t!e_v1.5.2.txt", want: Label{Repo: "go_sdk", Name: "src/cmd/go/testdata/mod/rsc.io_!q!u!o!t!e_v1.5.2.txt"}},
 		{str: "//:a][b", want: Label{Name: "a][b"}},
+		{str: "//:a b", want: Label{Name: "a b"}},
 	} {
 		got, err := Parse(tc.str)
 		if err != nil && !tc.wantErr {
