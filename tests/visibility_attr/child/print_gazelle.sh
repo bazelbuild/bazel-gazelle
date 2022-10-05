a977f016e5daa68d1e6940216d0ce33d96f0fd79
Chuck Grindel <chuck@chuckgrindel.com>
2022-10-05 09:12:37 -0600

    fix: pass gazelle visibility attribute to named sh_binary

---
+++ tests/visibility_attr/child/print_gazelle.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -o nounset -o pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -o errexit
+# --- end runfiles.bash initialization v2 ---
+
+gazelle_location=bazel_gazelle/tests/visibility_attr/gazelle
+gazelle="$(rlocation "${gazelle_location}")" || \
+  (echo >&2 "Failed to locate ${gazelle_location}" && exit 1)
+
+echo "${gazelle}"
---
+++ tests/visibility_attr/child/print_gazelle_test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -o nounset -o pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+# shellcheck disable=SC1090
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -o errexit
+# --- end runfiles.bash initialization v2 ---
+
+# MARK - Locate Deps
+
+print_gazelle_location=bazel_gazelle/tests/visibility_attr/child/print_gazelle
+print_gazelle="$(rlocation "${print_gazelle_location}")" || \
+  (echo >&2 "Failed to locate ${print_gazelle_location}" && exit 1)
+
+# MARK - Test
+
+output="$("${print_gazelle}")"
+[[ "${output}" =~ "gazelle" ]] || (echo >&2 "Did not find 'gazelle' in output."; exit 1)
