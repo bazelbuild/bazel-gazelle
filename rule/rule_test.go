dc7aa9c6d949d7a71f00bac11f853d16feadd000
Steve Barrau <98589981+stevebarrau@users.noreply.github.com>
2023-03-13 14:26:37 +0000

    add negative test

--- rule/rule_test.go
+++ rule/rule_test.go
@@ -375,6 +375,20 @@ func TestShouldKeepExpr(t *testing.T) {
 `,
 			want: true,
 		}, {
+			desc: "before but not the correct prefix (keeping)",
+			src: `
+			# keeping this for now
+"s"
+`,
+			want: false,
+		}, {
+			desc: "before but not the correct prefix (no colon)",
+			src: `
+			# keep this around for the time being
+"s"
+`,
+			want: false,
+		}, {
 			desc: "suffix",
 			src: `
 "s" # keep
