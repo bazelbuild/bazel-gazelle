b2a81af01450d39b71ed51043a26f7f3320bf0c3
Shahms King <shahms@google.com>
2023-09-15 04:26:27 -0700

    address comments from #1597 (#1628)

--- language/go/config.go
+++ language/go/config.go
@@ -29,10 +29,9 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/bazelbuild/bazel-gazelle/internal/module"
-
 	"github.com/bazelbuild/bazel-gazelle/config"
 	gzflag "github.com/bazelbuild/bazel-gazelle/flag"
+	"github.com/bazelbuild/bazel-gazelle/internal/module"
 	"github.com/bazelbuild/bazel-gazelle/internal/version"
 	"github.com/bazelbuild/bazel-gazelle/language/proto"
 	"github.com/bazelbuild/bazel-gazelle/repo"
@@ -160,8 +159,7 @@ func (m testMode) String() string {
 	case fileTestMode:
 		return "file"
 	default:
-		log.Panicf("unknown mode %d", m)
-		return ""
+		return "unknown"
 	}
 }
 
