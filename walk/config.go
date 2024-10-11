c6b59783bdc4b3760d969975343fb8500d308bc1
Alex Torok <alex.torok@aurora.tech>
2024-10-11 16:53:17 +0000

    Warn when it looks like you're using gazelle:ignore instead of gazelle:exclude

--- walk/config.go
+++ walk/config.go
@@ -95,6 +95,9 @@ func (cr *Configurer) Configure(c *config.Config, rel string, f *rule.File) {
 				}
 				wcCopy.follow = append(wcCopy.follow, path.Join(rel, d.Value))
 			case "ignore":
+				if d.Value != "" {
+					log.Printf("the ignore directive does not take any arguments. Did you mean to use gazelle:exclude instead? %s arg: '# gazelle:ignore %s'", f.Path, d.Value)
+				}
 				wcCopy.ignore = true
 			}
 		}
